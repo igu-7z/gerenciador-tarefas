006c88898370d17d62794fede9d5fa2e44153a52
igu-7z <igor_carvalho040@hotmail.com>
2026-06-11 21:38:31 -0300

    Criando a entidade Task com anotacoes do JPA e Lombok

---
+++ src/test/java/com/github/igu_7z/gerenciador_tarefas/GerenciadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.igu_7z.gerenciador_tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
