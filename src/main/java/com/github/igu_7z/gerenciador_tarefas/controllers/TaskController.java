9e56e36d4045433995cffc8faedc5228258a1f5f
igu-7z <igor_carvalho040@hotmail.com>
2026-06-11 22:26:44 -0300

    Adicionado endpoint POST para cadastro de novas tarefas

--- src/main/java/com/github/igu_7z/gerenciador_tarefas/controllers/TaskController.java
+++ src/main/java/com/github/igu_7z/gerenciador_tarefas/controllers/TaskController.java
@@ -1,7 +1,10 @@
 package com.github.igu_7z.gerenciador_tarefas.controllers;
 
+import com.github.igu_7z.gerenciador_tarefas.models.Task;
 import com.github.igu_7z.gerenciador_tarefas.repositories.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +14,9 @@ public class TaskController {
 
     @Autowired
     private TaskRepository taskRepository;
+
+    @PostMapping
+    public Task criar(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
 }
--- src/main/java/com/github/igu_7z/gerenciador_tarefas/repositories/TaskRepository.java
+++ src/main/java/com/github/igu_7z/gerenciador_tarefas/repositories/TaskRepository.java
@@ -1,7 +1,7 @@
 package com.github.igu_7z.gerenciador_tarefas.repositories;
 
+import com.github.igu_7z.gerenciador_tarefas.models.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.scheduling.config.Task;
 import org.springframework.stereotype.Repository;
 
 @Repository
