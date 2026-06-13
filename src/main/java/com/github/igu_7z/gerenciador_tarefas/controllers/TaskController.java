d356e82fbdb0018b2631c13792f867ac5446be7b
igu-7z <igor_carvalho040@hotmail.com>
2026-06-12 21:32:32 -0300

    adicionar endpoint GET para listar todas as tarefas

--- src/main/java/com/github/igu_7z/gerenciador_tarefas/controllers/TaskController.java
+++ src/main/java/com/github/igu_7z/gerenciador_tarefas/controllers/TaskController.java
@@ -3,10 +3,9 @@ package com.github.igu_7z.gerenciador_tarefas.controllers;
 import com.github.igu_7z.gerenciador_tarefas.models.Task;
 import com.github.igu_7z.gerenciador_tarefas.repositories.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
@@ -18,5 +17,10 @@ public class TaskController {
     @PostMapping
     public Task criar(@RequestBody Task task) {
         return taskRepository.save(task);
+    }
+
+    @GetMapping
+    public List<Task> listar() {
+        return taskRepository.findAll();
     }
 }
