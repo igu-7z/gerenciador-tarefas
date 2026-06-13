7885c9f501c8b25b7984041907434c4f0ebc1d04
igu-7z <igor_carvalho040@hotmail.com>
2026-06-12 22:19:55 -0300

    Adicionar endpoint PUT para atualizar tarefa por ID

--- src/main/java/com/github/igu_7z/gerenciador_tarefas/controllers/TaskController.java
+++ src/main/java/com/github/igu_7z/gerenciador_tarefas/controllers/TaskController.java
@@ -27,12 +27,29 @@ public class TaskController {
     }
 
     @GetMapping("{/id}")
-    public Optional<Task> buscarPorId(@PathVariable Long id){
+    public Optional<Task> buscarPorId(@PathVariable Long id) {
         return taskRepository.findById(id);
     }
 
     @DeleteMapping("{/id}")
-    public void deletar(@PathVariable Long id){
+    public void deletar(@PathVariable Long id) {
         taskRepository.deleteById(id);
+    }
+
+    @PutMapping("{/id}")
+    public Task atualizar(@PathVariable Long id, @RequestBody Task taskAtualizada) {
+        Optional<Task> taskOptional = taskRepository.findById(id);
+
+        if (taskOptional.isPresent()) {
+            Task taskExistente = taskOptional.get();
+
+            taskExistente.setTitle(taskAtualizada.getTitle());
+            taskExistente.setDescription(taskAtualizada.getDescription());
+
+            return taskRepository.save(taskExistente);
+
+        } else {
+            return null;
+        }
     }
 }
